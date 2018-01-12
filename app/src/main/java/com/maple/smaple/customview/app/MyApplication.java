9f059835a39b381c25f8d97f60081bf9ec0899a4
高冠祺 <gaoguanqi@xiaoxinyong.com>
2018-01-12 19:41:30 +0800

    add..

--- app/src/main/java/com/maple/smaple/customview/app/MyApplication.java
+++ app/src/main/java/com/maple/smaple/customview/app/MyApplication.java
@@ -27,14 +27,16 @@ public class MyApplication extends Application{
 
     }
 
+    /**
+     * 全局下拉刷新控件配置
+     */
     static {
         //设置全局的Header构建器
         SmartRefreshLayout.setDefaultRefreshHeaderCreater(new DefaultRefreshHeaderCreater() {
             @Override
             public RefreshHeader createRefreshHeader(Context context, RefreshLayout layout) {
                 layout.setPrimaryColorsId(R.color.colorPrimary, android.R.color.white);//全局设置主题颜色
-                //return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
-                return new MaterialHeader(context).setShowBezierWave(true);
+                return new ClassicsHeader(context);//.setTimeFormat(new DynamicTimeFormat("更新于 %s"));//指定为经典Header，默认是 贝塞尔雷达Header
             }
         });
         //设置全局的Footer构建器
@@ -42,8 +44,7 @@ public class MyApplication extends Application{
             @Override
             public RefreshFooter createRefreshFooter(Context context, RefreshLayout layout) {
                 //指定为经典Footer，默认是 BallPulseFooter
-                //return new ClassicsFooter(context).setDrawableSize(20);
-                return new  BallPulseFooter(context).setSpinnerStyle(SpinnerStyle.Scale);
+                return new ClassicsFooter(context).setDrawableSize(20);
             }
         });
     }
--- app/src/main/java/com/maple/smaple/customview/smart/SmartActivity.java
+++ app/src/main/java/com/maple/smaple/customview/smart/SmartActivity.java
@@ -3,14 +3,24 @@ package com.maple.smaple.customview.smart;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.maple.smaple.customview.R;
+import com.maple.smaple.customview.decoration.DividerListItemDecoration;
+import com.maple.smaple.customview.jianshu.JianShuAdapter;
+import com.maple.smaple.customview.jianshu.bean.BannerBean;
+import com.maple.smaple.customview.jianshu.bean.ListBean;
+import com.maple.smaple.customview.jianshu.bean.MenuBean;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
 import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,12 +36,60 @@ public class SmartActivity extends AppCompatActivity{
     @BindView(R.id.recyclerview)
     RecyclerView recyclerview;
 
+    private LinearLayoutManager mLinearLayoutManager;
+    private JianShuAdapter mAdapter;
+
+    private List<BannerBean> mBannerData;
+    private List<MenuBean> mMenuData;
+    private List<ListBean> mListData;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smart);
         ButterKnife.bind(this);
+        initView();
+        initData();
+        initEvent();
+    }
 
+    private void initView() {
+        mLinearLayoutManager = new LinearLayoutManager(this);
+        recyclerview.addItemDecoration(new DividerListItemDecoration(this,LinearLayoutManager.VERTICAL));
+        mAdapter = new JianShuAdapter(this);
+        recyclerview.setLayoutManager(mLinearLayoutManager);
+        recyclerview.setAdapter(mAdapter);
+    }
+
+    private void initData() {
+        mBannerData = new ArrayList<>();
+        BannerBean mBannerBean1 = new BannerBean("https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2526095363,542110218&fm=173&s=9E804A87CA30E4CE529D4F8003006095&w=218&h=146&img.JPEG","百度1");
+        BannerBean mBannerBean2 = new BannerBean("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=3509703757,1230033114&fm=173&s=7488AABE4A1024C81D1EB1F20300501A&w=218&h=146&img.JPEG","百度2");
+        BannerBean mBannerBean3 = new BannerBean("https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=2238839426,1679103971&fm=173&s=A851A14ED440815F58ECC49A03005093&w=218&h=146&img.JPEG","百度3");
+        mBannerData.add(mBannerBean1);
+        mBannerData.add(mBannerBean2);
+        mBannerData.add(mBannerBean3);
+
+        mMenuData = new ArrayList<>();
+
+        for (int i = 0; i < 15; i++) {
+            MenuBean mMenuBean = new MenuBean("https://www.baidu.com/img/bd_logo1.png","百度"+i);
+            mMenuData.add(mMenuBean);
+        }
+
+        mListData = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            ListBean mListBean = new ListBean("标题"+i,"描述描述描述描述描述描述","https://www.baidu.com/img/bd_logo1.png");
+            mListData.add(mListBean);
+        }
+
+
+        mAdapter.setBannerData(mBannerData);
+        mAdapter.setMenuData(mMenuData);
+        mAdapter.setListData(mListData);
+    }
+
+    private void initEvent() {
         refreshLayout.setOnRefreshListener(new OnRefreshListener() {
             @Override
             public void onRefresh(RefreshLayout refreshlayout) {
@@ -54,6 +112,5 @@ public class SmartActivity extends AppCompatActivity{
                 finish();
             }
         });
-
     }
 }
